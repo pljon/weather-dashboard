5f21bbd3d04409d38d14b185e52032e2117baf41
Jonathan Plaras <jonplaras@gmail.com>
2022-12-10 18:32:38 -0500

    added html/css template

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// api key
+key = 'eadb20433e6df59a3eca6031b67b3de7'
+
+// api's to search
+var cityApi = `api.openweathermap.org/data/2.5/weather?q=London&appid=${key}`
+var latlonApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
